3234bbd6995b9eff08ae684f687f5537ac43d3c3
buffalu <85544055+buffalu@users.noreply.github.com>
2024-11-07 21:00:11 -0600

    Separate out broadcast + retransmit shredstream (#703)

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -300,6 +300,7 @@ pub struct ValidatorConfig {
     pub block_engine_config: Arc<Mutex<BlockEngineConfig>>,
     // Using Option inside RwLock is ugly, but only convenient way to allow toggle on/off
     pub shred_receiver_address: Arc<RwLock<Option<SocketAddr>>>,
+    pub shred_retransmit_receiver_address: Arc<RwLock<Option<SocketAddr>>>,
     pub tip_manager_config: TipManagerConfig,
     pub preallocated_bundle_cost: u64,
 }
@@ -377,6 +378,7 @@ impl Default for ValidatorConfig {
             relayer_config: Arc::new(Mutex::new(RelayerConfig::default())),
             block_engine_config: Arc::new(Mutex::new(BlockEngineConfig::default())),
             shred_receiver_address: Arc::new(RwLock::new(None)),
+            shred_retransmit_receiver_address: Arc::new(RwLock::new(None)),
             tip_manager_config: TipManagerConfig::default(),
             preallocated_bundle_cost: u64::default(),
         }
@@ -1512,6 +1514,7 @@ impl Validator {
             slot_status_notifier,
             vote_connection_cache,
             config.shred_receiver_address.clone(),
+            config.shred_retransmit_receiver_address.clone(),
         )
         .map_err(ValidatorError::Other)?;
 
@@ -1611,6 +1614,7 @@ impl Validator {
             block_engine_config: config.block_engine_config.clone(),
             relayer_config: config.relayer_config.clone(),
             shred_receiver_address: config.shred_receiver_address.clone(),
+            shred_retransmit_receiver_address: config.shred_retransmit_receiver_address.clone(),
         });
 
         Ok(Self {
